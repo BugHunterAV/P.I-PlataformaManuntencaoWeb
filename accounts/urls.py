d8cc5d4f649ece3fe49b992f5c38e38c89d57318
VICTOR BRUNO <victorbruno@MacBook-Air-de-VICTOR.local>
2026-04-01 19:51:37 -0300

    Adicionando arquivos

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import EmpresaViewSet, UsuarioViewSet, MeView
-
+from rest_framework.authtoken import views as auth_views
+path('api/accounts/login/', auth_views.obtain_auth_token, name='login'),
 router = DefaultRouter()
 router.register(r'empresas', EmpresaViewSet)
 router.register(r'usuarios', UsuarioViewSet)
@@ -14,4 +15,4 @@ urlpatterns = [
     path('auth/login/',   TokenObtainPairView.as_view(),  name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(),     name='token_refresh'),
     path('auth/me/',      MeView.as_view(),               name='auth_me'),
-]
+]
